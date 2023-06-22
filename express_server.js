5111b9a5c590de6b1c903a17c272923949561ebf
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-22 01:21:06 -0500

    Modified const templateVars line

--- express_server.js
+++ express_server.js
@@ -20,6 +20,7 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+//Single URL & shortened form
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
