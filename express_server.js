7b8791b0eff9d4c4024e70a4d23c8267cad4b0c8
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-22 00:23:33 -0500

    Added a route for /urls

--- express_server.js
+++ express_server.js
@@ -16,6 +16,11 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 //Sending HTML
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
