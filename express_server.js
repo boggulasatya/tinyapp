2796dc3ba701cd635420af05891a26903aecd0b9
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-07-02 13:29:51 -0500

    Implement user registration validation

--- express_server.js
+++ express_server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-const PORT = 3000; // default port 8080
+const PORT = 3000; // default port 3000
 //Set ejs as the view engine
+//const { getUserByEmail } = require("./helper");
 app.set("view engine", "ejs");
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+//Users Global object
+const users = [
+  {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+];
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,7 +49,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username };
+  const templateVars = { urls: urlDatabase, user: users.find(user => user.id === req.cookies.user_id) };
   res.render("urls_index", templateVars);
 });
 
@@ -61,26 +76,56 @@ app.post("/urls", (req, res) => {
 
 //Adding a GET Route to show the form
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies.username };
+  const templateVars = { urls: urlDatabase, user: users.find(user => user.id === req.cookies.user_id) };
   res.render("urls_new", templateVars);
 });
 
 //Single URL & shortened form-Redirect Short URLs
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username };
+  const templateVars = { urls: urlDatabase, user: users.find(user => user.id === req.cookies.user_id) };
   res.render("urls_show", templateVars);
 });
 //Updating url
 app.get('/urls/:id/edit', (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  res.render('edit', { id: id, longURL: longURL, username: req.cookies.username });
+  res.render('edit', { id: id, longURL: longURL, user: users.find(user => user.id === req.cookies.user_id) });
 });
+
 //Login
 app.get('/login', (req, res) => {
-  console.log(req.cookies.username);
-  const templateVars = { urls: urlDatabase, username: req.cookies.username };
+  const {email, password} = req.body;
+  const templateVars = { urls: urlDatabase, user: users.find(user => user.id === req.cookies.user_id) };
   res.render("login", templateVars);
+});
+
+//Register GET route handler
+app.get("/register", (req, res) => {
+  const templateVars = { user: req.cookies.user_id };
+  res.render("register", { user: templateVars.user });
+});
+
+function getUserByEmail(email, users) {
+  return users.find(user => user.email === email);
+}
+app.post("/register", (req, res) => {
+  //const userId = generateRandomString();
+  const { email, password } = req.body;
+  //const user = users.find((user) => user.email === email);
+  const foundUser = getUserByEmail(email, users);
+  if (foundUser) {
+    return res.status(400).send("Email already registered");
+  }
+  //create a new user object
+  const newUser = {
+    id: generateRandomString(),
+    email: email,
+    password: password,
+  };
+  users.push(newUser);
+  //Set user_id cookie and redirect to URLs page
+  res.cookie("user_id", newUser.id);
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -97,27 +142,26 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-const users = [
-  {id: '1', username: 'satya', password: 'password1'}
-];
-function isValidLogin(username, password) {
-  const user = users.find(user => user.username === username);
-  return user !== undefined;
-}
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
- 
-  if (isValidLogin(username, password)) {
-    res.cookie('username', username);
-    res.redirect("/urls");
-  } else {
-    res.send('Invaild username or password');
+  const { email, password } = req.body;
+  const foundUser = getUserByEmail(email, users);
+  // Find the user with the provided email
+  const user = users.find(user => user.email === email);
+  if (!foundUser) {
+    return res.status(403).send("User not found");
   }
+  // Check if the user exists and the password is correct
+  if (foundUser.password !== password) {
+    return res.status(403).send("Incorrect password");
+  }
+  res.cookie('user_id', user.id);
+  res.redirect("/urls");
 });
+
 //Logout
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/login");
 });
 
 //Sending HTML
